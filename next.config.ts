0f991c2e1e95e1ab61256a3808cf915026431c01
xuantruong091196 <theend96nd@gmail.com>
2025-11-22 17:09:27 +0700

    Fix

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
 	/* config options here */
 	async redirects() {
 		return [
 			{
-				// Source: Đường dẫn đến
 				source: "/",
-
-				// Destination: Đường dẫn sẽ được chuyển đến
 				destination: "/asset-vault",
-
-				// permanent: true => Trả về mã trạng thái 308 (Permanent Redirect)
-				// Đây là cách tốt cho SEO và trình duyệt sẽ lưu cache
 				permanent: true,
 			},
-			// Bạn có thể thêm nhiều redirects khác ở đây
 		];
 	},
-=======
-  /* config options here */
->>>>>>> main
 };
 
 export default nextConfig;
